6a9e740d9d2771c235074d583dd37ec3ada6714e
marcelloprado <93588409+marcelloprado@users.noreply.github.com>
2021-12-07 22:18:17 -0300

    Create etapas.js
    Json

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Homer Simpson',
+                partido: 'Bar do Moe',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Bart Simpson',
+                partido: 'Springfield',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos: [
+            {
+                numero: '99',
+                nome:'Moe Szyslak',
+                partido: 'Bar do Moe',
+                vice: 'Barney Gumble',
+                fotos:[
+                    {url:'99.jpg', legenda:'Prefeito'},
+                    {url:'99.2.jpg', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '84',
+                nome:'Diretor Skinner',
+                partido: 'Elementar de Springfield',
+                vice: 'Zelador Willie',
+                fotos:[
+                    {url:'84.jpg', legenda:'Prefeito'},
+                    {url:'84.2.jpg', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+            
+        ]
+    }
+]
