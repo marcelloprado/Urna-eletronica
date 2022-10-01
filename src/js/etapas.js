f638b51cbfe9c3b23ca206503db6e67e0544c57e
Marcello Prado <marcellopradomuller@gmail.com>
2022-10-01 08:17:12 -0300

    criando pasta reset e variables, ajustando css

--- src/js/etapas.js
+++ src/js/etapas.js
@@ -5,16 +5,16 @@ let etapas = [
         candidatos: [
             {
                 numero: '38111',
-                nome: 'Homer Simpson',
-                partido: 'Bar do Moe',
+                nome: 'Diretor Skinner',
+                partido: 'Elementar de Springfield',
                 fotos: [
                     { url: '../src/images/38111.jpg', legenda: 'Deputado' }
                 ]
             },
             {
                 numero: '77222',
-                nome: 'Ned Flanders',
-                partido: 'Primeira igreja de Springfield',
+                nome: 'Otto Mann',
+                partido: 'Rock´n Roll',
                 fotos: [
                     { url: '../src/images/77222.jfif', legenda: 'Deputado' }
                 ]
@@ -27,19 +27,20 @@ let etapas = [
         candidatos: [
             {
                 numero: '99',
-                nome: 'Moe Szyslak',
-                partido: 'Bar do Moe',
-                vice: 'Barney Gumble',
+                nome: 'Ned Flanders',
+                partido: 'Primeira igreja de Springfield',
+
+                vice: 'Reverendo Thimoteo',
                 fotos: [
-                    { url: '../src/images/99.jpg', legenda: 'Presidente' },
-                    { url: '../src/images/99.2.jpg', legenda: 'Vice-Presidente', small: true }
+                    { url: '../src/images/99.jfif', legenda: 'Presidente' },
+                    { url: '../src/images/99.2.jfif', legenda: 'Vice-Presidente', small: true }
                 ]
             },
             {
                 numero: '84',
-                nome: 'Diretor Skinner',
-                partido: 'Elementar de Springfield',
-                vice: 'Zelador Willie',
+                nome: 'Homer Simpson',
+                partido: 'Bar do Moe',
+                vice: 'Moe Szyslak',
                 fotos: [
                     { url: '../src/images/84.jpg', legenda: 'Presidente' },
                     { url: '../src/images/84.2.jpg', legenda: 'Vice-Presidente', small: true }
